b9a05816c72badea2161b4f4346117ee2fe25a9b
Space Around <viksnamax@mail.ru>
2020-11-26 02:26:12 +0300

    Edit comments

--- studentExample.js
+++ studentExample.js
@@ -1,41 +1,54 @@
-let student = new GuestVC();
+let student = new GuestVC(), // GuestVC instance
+    roomIDElem = document.getElementById('validationTooltip01'); // input with room ID
 
+// ussing default button control state (definition below)
 defaultControlState();
 
-student.setElementHTMLVideoContainerRemote(document.querySelector("#ssb"), document.getElementById('teacher-video'));
+// set HTML elems where the admin camera and screen will be displayed
+student.setElementHTMLVideoContainerRemote(document.querySelector("#ssb"), //screen
+                                           document.getElementById('teacher-video')); //camera
 
+// set HTML elems where the own camera will be displayed
 student.setElementHTMLVideoContainerLocal(document.getElementById('student-video'));
 
+// set button status when connecting and diconnection
 student.setConnectBtnUI(function() {
+    // connection
     document.getElementById('connect').removeAttribute("disabled");
     document.getElementById('connect').classList.add("btn-primary");
     document.getElementById('connect').innerHTML = "Подключиться";
     document.getElementById('connect').classList.remove("btn-danger");
 },
 function() {
+    // disconnection
     document.getElementById('connect').removeAttribute("disabled");
     document.getElementById('connect').classList.remove("btn-primary");
     document.getElementById('connect').innerHTML = "Отключиться";
     document.getElementById('connect').classList.add("btn-danger");
 });
 
+// handler button click for connection/diconnectiong
 document.getElementById('connect').onclick = function() {
-
+    // check connection status
     if(!student.getUserStatusConnection()) {
-        let roomID = document.getElementById('validationTooltip01').value;
+        // if disconnect
+        let roomID = roomIDElem.value;
         student.setRoomID(roomID);
         student.connect();
     } else {
+        // if connect
         student.disconnect();
     }
 
 }
 
+// set default handler for webrtc class
 student.onUserStatusChanged();
 student.onRoomFull();
 student.onStream();
 student.onMediaError();
 
+// define default button control state
 function defaultControlState() {
     document.querySelector('.micro > svg:nth-child(1)').style.display = "block";
     document.querySelector('.micro > svg:nth-child(2)').style.display = "none";
@@ -49,58 +62,72 @@ function defaultControlState() {
     document.querySelector('.chat > svg:nth-child(1)').style.display = "none";
 }
 
-// let micro = false,
-//     camera = false,
-//     chat = true;
-//
-// document.querySelector('.micro').onclick = function(e){
-//     micro = !micro;
-//     if (micro) {
-//         document.querySelector('.micro > svg:nth-child(2)').style.display = "block";
-//         document.querySelector('.micro > svg:nth-child(1)').style.display = "none";
-//
-//         connection.streamEvents[connection.attachStreams[0].streamid].stream.mute("audio");
-//     } else {
-//         document.querySelector('.micro > svg:nth-child(2)').style.display = "none";
-//         document.querySelector('.micro > svg:nth-child(1)').style.display = "block";
-//
-//         connection.streamEvents[connection.attachStreams[0].streamid].stream.unmute("audio");
-//     }
-//
-// }
-//
-// document.querySelector('.camera').onclick = function(e){
-//     camera = !camera;
-//     if (camera) {
-//         document.querySelector('.camera > svg:nth-child(2)').style.display = "block";
-//         document.querySelector('.camera > svg:nth-child(1)').style.display = "none";
-//
-//         connection.streamEvents[connection.attachStreams[0].streamid].stream.mute("video");
-//
-//         console.log("mute");
-//     } else {
-//         document.querySelector('.camera > svg:nth-child(2)').style.display = "none";
-//         document.querySelector('.camera > svg:nth-child(1)').style.display = "block";
-//
-//         connection.streamEvents[connection.attachStreams[0].streamid].stream.unmute("video");
-//
-//         console.log("unmute");
-//     }
-//
-// }
-//
-// document.querySelector('.chat').onclick = function(e){
-//     if (chat) {
-//         document.querySelector('.chat > svg:nth-child(1)').style.display = "block";
-//         document.querySelector('.chat > svg:nth-child(2)').style.display = "none";
-//
-//         document.querySelector('.overlay-chat').style.display = "block";
-//     } else {
-//         document.querySelector('.chat > svg:nth-child(1)').style.display = "none";
-//         document.querySelector('.chat > svg:nth-child(2)').style.display = "block";
-//
-//         document.querySelector('.overlay-chat').style.display = "none";
-//     }
-//
-//     chat = !chat;
-// }
+// define default button control state as variable
+let micro = false,
+    camera = false,
+    chat = true;
+
+// handler button for micro
+document.querySelector('.micro').onclick = function(e){
+    micro = !micro;
+    if (micro) {
+
+        // disable
+        document.querySelector('.micro > svg:nth-child(2)').style.display = "block";
+        document.querySelector('.micro > svg:nth-child(1)').style.display = "none";
+
+        student.microOff();
+    } else {
+
+        // enable
+        document.querySelector('.micro > svg:nth-child(2)').style.display = "none";
+        document.querySelector('.micro > svg:nth-child(1)').style.display = "block";
+
+        student.microOn();
+    }
+
+}
+
+// handler button for camera
+document.querySelector('.camera').onclick = function(e){
+    camera = !camera;
+    if (camera) {
+
+        // disable
+        document.querySelector('.camera > svg:nth-child(2)').style.display = "block";
+        document.querySelector('.camera > svg:nth-child(1)').style.display = "none";
+
+        student.localCameraOff();
+
+    } else {
+
+        // enable
+        document.querySelector('.camera > svg:nth-child(2)').style.display = "none";
+        document.querySelector('.camera > svg:nth-child(1)').style.display = "block";
+
+        student.localCameraOn();
+
+    }
+
+}
+
+// handler button for chat
+document.querySelector('.chat').onclick = function(e){
+    if (chat) {
+
+        // disable
+        document.querySelector('.chat > svg:nth-child(1)').style.display = "block";
+        document.querySelector('.chat > svg:nth-child(2)').style.display = "none";
+
+        document.querySelector('.overlay-chat').style.display = "block";
+    } else {
+
+        // enable
+        document.querySelector('.chat > svg:nth-child(1)').style.display = "none";
+        document.querySelector('.chat > svg:nth-child(2)').style.display = "block";
+
+        document.querySelector('.overlay-chat').style.display = "none";
+    }
+
+    chat = !chat;
+}
--- teacherExample.js
+++ teacherExample.js
@@ -1,44 +1,56 @@
-let teacher = new AdminVC(),
-    roomIDElem = document.getElementById('validationTooltip01'),
-    maxGuestCount = 4;
+let teacher = new AdminVC(), // AdminVC instance
+    roomIDElem = document.getElementById('validationTooltip01'), // input with room ID
+    maxGuestCount = 4; // max guest count
 
+// ussing default button control state (definition below)
 defaultControlState();
 
-teacher.setElementsHTMLVideoContainerLocal(document.getElementById('video-1'),
-                                           document.querySelector('.screen-share-block'));
+// set HTML elems where the own camera and screen will be displayed
+teacher.setElementsHTMLVideoContainerLocal(document.getElementById('video-1'), // camera
+                                           document.querySelector('.screen-share-block')); //screen
 
+// set HTML elems where the guest camera will be displayed
 teacher.setElementsHTMLVideoContainerRemote(document.getElementById('video-2'),
                                             document.getElementById('video-3'),
                                             document.getElementById('video-4'));
+
+// set button status when connecting and diconnection
 teacher.setConnectBtnUI(function() {
+    // connection
     document.getElementById('connect').removeAttribute("disabled");
     document.getElementById('connect').classList.add("btn-primary");
     document.getElementById('connect').innerHTML = "Подключиться";
     document.getElementById('connect').classList.remove("btn-danger");
 }, function() {
+    // disconnection
     document.getElementById('connect').removeAttribute("disabled");
     document.getElementById('connect').classList.remove("btn-primary");
     document.getElementById('connect').innerHTML = "Отключиться";
     document.getElementById('connect').classList.add("btn-danger");
 });
 
+// handler button click for connection/diconnectiong
 document.getElementById('connect').onclick = function() {
-
+    // check connection status
     if(!teacher.getUserStatusConnection()) {
+        // if disconnect
         let roomID = roomIDElem.value;
         teacher.setRoomID(roomID);
         // teacher.setMaxGuestCount(maxGuestCount);
         teacher.connect();
     } else {
+        // if connect
         teacher.disconnect();
     }
 
 }
 
+// set default handler for webrtc class
 teacher.onUserStatusChanged();
 teacher.onStream();
 teacher.onMediaError();
 
+// define default button control state
 function defaultControlState() {
     document.querySelector('.micro > svg:nth-child(1)').style.display = "block";
     document.querySelector('.micro > svg:nth-child(2)').style.display = "none";
@@ -55,19 +67,23 @@ function defaultControlState() {
     document.querySelector('.chat > svg:nth-child(1)').style.display = "none";
 }
 
+// define default button control state as variable
 let micro = false,
     camera = false,
     chat = true,
     screen_share = false;
 
+// handler button for micro
 document.querySelector('.micro').onclick = function(e){
     micro = !micro;
     if (micro) {
+        // disable
         document.querySelector('.micro > svg:nth-child(2)').style.display = "block";
         document.querySelector('.micro > svg:nth-child(1)').style.display = "none";
 
         teacher.microOff();
     } else {
+        // enable
         document.querySelector('.micro > svg:nth-child(2)').style.display = "none";
         document.querySelector('.micro > svg:nth-child(1)').style.display = "block";
 
@@ -76,14 +92,17 @@ document.querySelector('.micro').onclick = function(e){
 
 }
 
+// handler button for camera
 document.querySelector('.camera').onclick = function(e){
     camera = !camera;
     if (camera) {
+        // disable
         document.querySelector('.camera > svg:nth-child(2)').style.display = "block";
         document.querySelector('.camera > svg:nth-child(1)').style.display = "none";
 
         teacher.localCameraOff();
     } else {
+        // enabale
         document.querySelector('.camera > svg:nth-child(2)').style.display = "none";
         document.querySelector('.camera > svg:nth-child(1)').style.display = "block";
 
@@ -92,13 +111,16 @@ document.querySelector('.camera').onclick = function(e){
 
 }
 
+// handler button for chat
 document.querySelector('.chat').onclick = function(e){
     if (chat) {
+        // disable
         document.querySelector('.chat > svg:nth-child(1)').style.display = "block";
         document.querySelector('.chat > svg:nth-child(2)').style.display = "none";
 
         document.querySelector('.overlay-chat').style.display = "block";
     } else {
+        // enable
         document.querySelector('.chat > svg:nth-child(1)').style.display = "none";
         document.querySelector('.chat > svg:nth-child(2)').style.display = "block";
 
@@ -108,8 +130,10 @@ document.querySelector('.chat').onclick = function(e){
     chat = !chat;
 }
 
+// handler button for screen sharing
 document.querySelector('.screen-share').onclick = function(e){
     if (screen_share) {
+        // disable
         document.querySelector('.screen-share > svg:nth-child(2)').style.display = "block";
         document.querySelector('.screen-share > svg:nth-child(1)').style.display = "none";
 
@@ -117,6 +141,7 @@ document.querySelector('.screen-share').onclick = function(e){
         teacher.screenShareOff();
 
     } else {
+        // enbale
         document.querySelector('.screen-share > svg:nth-child(2)').style.display = "none";
         document.querySelector('.screen-share > svg:nth-child(1)').style.display = "block";
 
